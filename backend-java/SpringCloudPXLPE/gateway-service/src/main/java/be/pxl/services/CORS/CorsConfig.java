9f821cb3c95453c0db22ba8e39957cac38066bd4
BrechtMaesPXL <92795017+BrechtMaesPXL@users.noreply.github.com>
2025-01-10 19:13:38 +0100

    BUP

---
+++ backend-java/SpringCloudPXLPE/gateway-service/src/main/java/be/pxl/services/CORS/CorsConfig.java
@@ -0,0 +1,25 @@
+package be.pxl.services.CORS;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("*"); // Pas aan indien nodig
+        corsConfig.addAllowedMethod("*");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.setAllowCredentials(false);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+}
